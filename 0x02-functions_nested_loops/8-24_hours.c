dc95716c6a6946d5ab5927dd3f3e0ed76beabd6d
pere isenah <pereisenah@gmail.com>
2022-10-19 00:27:00 +0100

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,16 +7,32 @@
 void jack_bauer(void)
 {
 int hr, min;
-for (hr = 0; hr <= 23; hr++)
+for (hr = 0; hr <= 24; hr++)
 {
-for (min = 0; min <= 59; min++)
+for (min = 0; min <= 60; min++)
+{
+if (hr < 10)
+{
+_putchar('0');
+_putchar(hr + '0');
+}
+else if (hr >= 10)
 {
 _putchar((hr / 10) + '0');
 _putchar((hr % 10) + '0');
+}
+if (min < 10)
+{
+_putchar(':');
+_putchar('0');
+_putchar(min + '0');
+}
+else if (min > 10)
+{
 _putchar(':');
 _putchar((min / 10) + '0');
-_putchar((min / 10) + '0');
-_putchar('\n');
+_putchar((min % 10) + '0');
+}
 }
 }
 }
